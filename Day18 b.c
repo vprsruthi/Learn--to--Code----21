8af23b27cd26ed415ded82d6e34ff993548bc01d
vprsruthi <83950367+vprsruthi@users.noreply.github.com>
2021-06-03 20:56:16 +0530

    SUM CHECK

---
+++ Day18 b.c
@@ -0,0 +1,11 @@
+//Write a C program to input two numbers.If sum of the numbers is even, find the reverse of the sum .If sum is odd check whether the sum is prime or not.
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+     
+    return 0;
+}
