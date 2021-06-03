e20cdaebe9b371fdff8421fc0ed5f7f097e47049
vprsruthi <83950367+vprsruthi@users.noreply.github.com>
2021-06-03 20:33:36 +0530

    Build a Half Pyramid

---
+++ Day17 a.c
@@ -0,0 +1,26 @@
+Write a C program to print half pyramid of numbers
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int i,j,n;
+    scanf("%d",&n);
+    for(i=1;i<=n; i++)
+    {
+       for(j=1;j<=i; j++) 
+       { 
+           printf("%d ",j);
+           
+       }
+        printf("\n");
+    }
+    if (n<1)
+    {
+        printf("ERROR");
+    }
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
