54427c96c3f009e836bdc566229b8d4621fffde5
vprsruthi <83950367+vprsruthi@users.noreply.github.com>
2021-06-03 20:53:38 +0530

    SWAP 22

---
+++ Day18a.c
@@ -0,0 +1,29 @@
+//Write a C program to swap the values of two variables.
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+
+    void swap (int,int);
+    int main()
+    { 
+    int a,b;
+scanf("%d%d",&a,&b);
+printf("a=%d b=%d\n",a,b);
+swap(a,b);
+        
+
+}
+ void swap(int x, int y)
+ { 
+printf("a=%d b=%d",y,x);
+ 
+       
+
+
+
+      
+    return ;
+}
